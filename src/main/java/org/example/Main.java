caf29bdd33d8e03d9b2ec5046982c76c555bb30e
Antonio Mas <antoniomas33@hotmail.com>
2023-02-23 11:39:57 +0100

    prueba archivo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,23 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package org.example;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.nio.file.*;
+
+public class Main {
+
+      private static File archivo = new File("pruebafichero_backend/texto.csv");
+
+    public static void main(String[] args) throws IOException {
+
+
+
+        //Libreria libreria = new Libreria(RUTA);
+        Libreria.escribirFichero();
+
+
     }
 }
